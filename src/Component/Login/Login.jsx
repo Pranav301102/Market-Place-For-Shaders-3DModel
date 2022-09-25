75c5657be381fae99ec3c820cbbfdd1899a53c0f
Sahil8383 <95122593+Sahil8383@users.noreply.github.com>
2022-09-25 18:22:14 +0530

    Css Chanegs

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -14,7 +14,7 @@ import './Login.css';
 function Login() {
   return (
   <>
-    <Container size={420} my={40}>
+    <Container size={420} my={40} className="cont" >
       <Title
         align="center"
         sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
@@ -28,7 +28,7 @@ function Login() {
         </Anchor>
       </Text>
 
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md" >
         <TextInput label="Email" placeholder="you@mantine.dev" required />
         <PasswordInput label="Password" placeholder="Your password" required mt="md" />
         <Group position="apart" mt="md">
