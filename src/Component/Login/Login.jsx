1097ddae8d4125062890f263f087af6d8b5a395e
Pranav <pranavtrivedi02@gmail.com>
2022-09-26 14:00:27 +0530

    Added Pages

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -9,39 +9,55 @@ import {
   Container,
   Group,
   Button,
-} from '@mantine/core';
-import './Login.css';
+} from "@mantine/core";
+import "./Login.css";
 function Login() {
   return (
-  <>
-    <Container size={420} my={40} className="cont" >
-      <Title
-        align="center"
-        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-      >
-        Welcome back!
-      </Title>
-      <Text color="dimmed" size="sm" align="center" mt={5}>
-        Do not have an account yet?{' '}
-        <Anchor href="#" size="sm" onClick={(event) => event.preventDefault()}>
-          Create account
-        </Anchor>
-      </Text>
-
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md" >
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
-        <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-        <Group position="apart" mt="md">
-          <Checkbox label="Remember me" />
-          <Anchor onClick={(event) => event.preventDefault()} href="#" size="sm">
-            Forgot password?
+    <>
+      <Container size={420} my={40} className="cont">
+        <Title
+          align="center"
+          sx={(theme) => ({
+            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+            fontWeight: 900,
+          })}
+        >
+          Welcome back!
+        </Title>
+        <Text color="dimmed" size="sm" align="center" mt={5}>
+          Do not have an account yet?{" "}
+          <Anchor
+            href="#"
+            size="sm"
+            onClick={(event) => event.preventDefault()}
+          >
+            Create account
           </Anchor>
-        </Group>
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
-      </Paper>
-    </Container>
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <TextInput label="Email" placeholder="you@mantine.dev" required />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+          />
+          <Group position="apart" mt="md">
+            <Checkbox label="Remember me" />
+            <Anchor
+              onClick={(event) => event.preventDefault()}
+              href="#"
+              size="sm"
+            >
+              Forgot password?
+            </Anchor>
+          </Group>
+          <Button fullWidth mt="xl">
+            Sign in
+          </Button>
+        </Paper>
+      </Container>
     </>
   );
 }
